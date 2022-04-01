682896da4cff908720ca9e2c96fd0268c61ca05c
hueylu910201 <hueylu910201@gmail.com>
2022-04-01 16:41:06 +0800

    test5

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your App test!</Text>
+      <Text>Open up App.js to start working on your App test5!</Text>
       <StatusBar style="auto" />
     </View>
   );
