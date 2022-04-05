ccbac6d370d414a5c8ac888489399398bed21081
Yuder <a815261s@gmail.com>
2022-04-05 18:14:42 +0800

    米堤內頁(初)+list/愛心

--- src/components/DrinkDetail.js
+++ src/components/DrinkDetail.js
@@ -1,40 +1,80 @@
-import React from "react";
+import React ,{useState}from "react";
 import { Box, HStack, VStack, AspectRatio, Text, Image, Pressable } from "native-base"
+import  Starrating from "./Starrating";
+import {TouchableOpacity} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const DrinkDetail = ({ drink, navigation }) => {
+
+  const [toggle, setToggle] = useState(true);
+    const toggleFunction = () => {
+        setToggle(!toggle);
+    };
   return (
-    <Box 
-      marginX={1} marginBottom={2} borderRadius={3} shadow={2}
+    <Box padding={3} 
+      borderWidth={2} borderRightRadius={5} borderColor="#dcebfa"
+      marginX={1} marginBottom={2} borderRadius={10} shadow={2}
       _dark={{ borderColor: 'blueGray.500', borderWidth: 0.6 }}
-    >
+      _light={{ bg: "#eff6fd" }}>
+      <Pressable onPress={() => navigation.navigate('Detail', drink)}>
       <HStack 
         _dark={{ bg: "blueGray.900"}}
-        _light={{ bg: "white" }}>
-        <AspectRatio w="50" ratio={1}>
+      >
+        <AspectRatio w="100" ratio={1}>
           <Image
             margin="1"
             source={{ uri: drink.thumbnail_image }}
             alt="artist"
           />
         </AspectRatio>
-        <VStack paddingLeft={2} justifyContent="space-around">
-          <Text>{drink.title}</Text>
-          <Text>{drink.artist}</Text>
+        <VStack paddingLeft={2} >
+          <HStack>
+            <Text fontSize={26} fontWeight="700">{drink.title}</Text>
+            <TouchableOpacity onPress={() => toggleFunction()} >
+                <Box name={'heart-outline'} color={'red'}>
+                  <Text paddingLeft={3} paddingTop="2.5"  >{toggle ? 
+             
+                    <MaterialCommunityIcons name={'heart-outline'} color={'black'}  size={25} />:
+                    <MaterialCommunityIcons name={'heart'} color={'#ff5e5e'} size={25} />}
+                  
+                   </Text>
+                </Box>    
+              </TouchableOpacity>
+          </HStack>
+          <Text fontSize={18}>{drink.address}</Text>
+          <HStack marginTop={2}>
+            <Starrating  value={drink.rate}/>
+            <Text paddingLeft="5%" marginTop="-2%" fontSize={15}>{drink.score}</Text>
+          </HStack>
+          
+
         </VStack>
       </HStack>
-      <Box p={1} _dark={{ bg: "blueGray.900" }}
+      </Pressable>
+      {/* <Box p={1} _dark={{ bg: "blueGray.900" }}
         _light={{ bg: "white" }}>
-        <Pressable 
-          onPress={() => navigation.navigate('Detail', drink)}
-        >
+        
+        
           <AspectRatio w="100%" ratio={1}>
             <Image
               source={{ uri: drink.image }}
               alt="drink"
             />            
+
+          *****<Pressable 
+          onPress={() => navigation.navigate('Detail', album)}
+        >
+          <AspectRatio w="100%" ratio={1}>
+            <Image
+              source={{ uri: album.image }}
+              alt="album"
+            />            
+          </AspectRatio>
+        </Pressable>*****
+
           </AspectRatio>
         </Pressable>
-      </Box>   
+      </Box>    */}
     </Box>
   )};
 
--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Linking } from 'react-native';
-import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button } from "native-base";
+import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button, VStack ,color} from "native-base";
+import Starrating_detail from "../components/Starrating_detail"
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-
-const DetailScreen = ({ route }) => {
-  const { title, 
+const DetailScreen = ({ route , drink }) => {
+  const { 
+    title, 
     artist,
-    price,
     url,
     image,
-    description
+    description,
+    rate,
+    score,
+    address,
+    time,
+    tel
   } = route.params;
   return (
     <Center 
@@ -26,9 +32,23 @@ const DetailScreen = ({ route }) => {
         <Box shadow={1} _dark={{ bg: "blueGray.900", borderColor: 'blueGray.500', borderWidth: 0.6 }}
         _light={{ bg: "blueGray.50" }} padding="2" margin="2">
           <Center>
-            <Heading pt={1} fontSize="2xl" color='#6099E4'>Discount Now!</Heading>
-            <Heading fontSize="4xl">Price: ${price}</Heading>
+            
+            <Heading pt={1} fontSize="4xl" color='#6099E4' marginLeft={"-60%"}>{title}</Heading>
+            <Box marginTop={"-9%"} marginLeft={"30%"} >
+              <Starrating_detail value={rate} />
+            </Box>
+            <Text fontSize={"20"} marginLeft={"87%"} fontWeight={"600"} marginTop={"-7%"}>{score}</Text>
+            
+            {/* <Heading fontSize="4xl">Price: ${price}</Heading> */}
+            <VStack marginLeft={"-30%"} marginTop={"5%"}  >
+
+              <Text fontSize={"18"} >地址:{address}</Text>
+              <Text fontSize={"18"} >電話:{tel}</Text>
+              <Text fontSize={"18"} >營業時間:{time}</Text>
+            </VStack>
+            <MaterialCommunityIcons  name="star" color={color} size={45} />
           </Center>
+          
           <Button 
             mt="4"
             onPress={() => Linking.openURL(url)}
